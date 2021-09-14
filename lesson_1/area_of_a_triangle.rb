32b9a93bb7c60cb72012ab165132f72126c11b9d
AlexanderG-v <gorelikov.alexander@gmail.com>
2021-09-14 12:12:37 +0300

    Second task

---
+++ lesson_1/area_of_a_triangle.rb
@@ -0,0 +1,11 @@
+
+# - площадь треугольника
+
+puts "Let's calculate the area of the triange?"
+sleep 2
+puts "What is length of base of the triangle in sm?"
+base = gets.chomp
+puts "Now indicate height of the triandge in sm"
+height = gets.chomp
+
+puts "The area of the triagle is #{0.5 * base.to_i * height.to_i} sm."
