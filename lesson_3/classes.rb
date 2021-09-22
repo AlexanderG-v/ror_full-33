00477f4bca6023bade030eaafd52d4ac8ff60492
AlexanderG-v <gorelikov.alexander@gmail.com>
2021-09-22 19:36:39 +0300

    created class Route

--- lesson_3/classes.rb
+++ lesson_3/classes.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 
 class Station
   attr_reader :trains
@@ -10,5 +11,26 @@ class Station
   def arrival_train(name_train)
     @trains << name_train
   end
+end
 
+class Route
+  def initialize(starting_station, ending_station)
+    @intermediate_station = [starting_station, ending_station]
+  end
+
+  # - повторяется в классе Станций
+  def adding_stations(name_station)
+    @intermediate_station.insert(-2, name_station)
+  end
+
+  def delete_station(name_station)
+    @intermediate_station.delete(name_station)
+  end
+
+  def show_stations
+    @intermediate_station.each { |name| puts name }
+  end
+end
+
+class Train
 end
