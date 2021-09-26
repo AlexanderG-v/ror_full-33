d0760bd588c633b42421206277163d2df4263aed
AlexanderG-v <gorelikov.alexander@gmail.com>
2021-09-26 15:33:47 +0300

    created files route.rb, station.rb, trait.rb

---
+++ lesson_4/route.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+class Route
+  # - При создании экземпляра класса указывается начальная и конечная станции
+  attr_reader :stations
+
+  def initialize(first_station, last_station)
+    @stations = [first_station, last_station]
+  end
+
+  # - добовляет промежуточную станцию в список
+  def add_stations(station)
+    @stations.insert(-2, station)
+  end
+
+  # - удаляет промежуточную станцию из списка
+  def delete_station(station)
+    @stations.delete(station)
+  end
+
+  # - выводит список всех станций по-порядку от начала до конца
+  def show_stations
+    @stations.each { |station| puts station.name.to_s }
+  end
+
+  # - при назначении маршрута поезду, поезд автоматически помещается на первую станцию в маршруте
+  def add_train(train)
+    @stations[0].arrival(train)
+  end
+end
---
+++ lesson_4/train.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+class Train
+  # - при создании экземпляра класса указывается номер (произвольная строка) и тип (грузовой, пассажирский) и кол-во вагонов
+  attr_accessor :current_speed, :wagons
+  attr_reader :type
+
+  def initialize(number, type, wagons)
+    @number = number
+    @type = type
+    @wagons = wagons.to_i
+    @current_speed = 0
+  end
+
+  # - набирает скорость
+  def speed_up(speed)
+    self.current_speed += speed
+  end
+
+  # - тормозит (сбрасывает скорость до нуля)
+  def stop
+    self.current_speed = 0
+  end
+
+  # - прицепляет вагоны(по одному вагону за операцию) при условии, что поезд не движется
+  def add_wagons
+    self.wagons += 1 if self.current_speed.zero?
+  end
+
+  # - отцепляет вагоны(по одному вагону за операцию) при условии, что поезд не движется
+  def remove_wagons
+    self.wagons -= 1 if self.current_speed.zero?
+  end
+
+  # - принимает маршрут следования (экземпляр класса Route)
+  # - при назначении маршрута поезду, поезд автоматически помещается на первую станцию в маршруте
+  def add_route(route)
+    @route = route
+    @current_station = @route.stations[0]
+    @route.add_train(self)
+  end
+
+  # - возвращает предыдущую станцию, на основе маршрута
+  def previous_station
+    @route.stations[@route.stations.index(@current_station) - 1] if @current_station != @route.stations.first
+  end
+
+  # - возвращает следующую станцию, на основе маршрута
+  def next_station
+    @route.stations[@route.stations.index(@current_station) + 1] if @current_station != @route.stations.last
+  end
+
+  # - перемещение вперед между станциями, но только на одну станцию за раз
+  def go_forward
+    return unless next_station
+
+    @current_station.departure(self)
+    @current_station = next_station
+    @current_station.arrival(self)
+  end
+
+  # - перемещение назад между станциями, но только на одну станцию за раз
+  def go_back
+    return unless previous_station
+
+    @current_station.departure(self)
+    @current_station = previous_station
+    @current_station.arrival(self)
+  end
+end
