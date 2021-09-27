e903c261cf9b00c87a109b9b2cfcf0485d3f389c
AlexanderG-v <gorelikov.alexander@gmail.com>
2021-09-27 12:47:24 +0300

    mod created TUI,user input

--- lesson_4/test.rb
+++ lesson_4/test.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Main
-  attr_reader :trains, :stations, :route, :wagons
+  attr_accessor :trains, :stations, :route, :wagons
 
   def initialize
     @stations = []
@@ -21,7 +21,28 @@ class Main
   Введите 8, что бы отцепить вагоны от поезда.
   Введите 9, что бы отправить поезд по маршруту вперед.
   Введите 10, что бы отправить поезд по маршруту назад.
-  Введите 11, что бы просмотреть список станций и список поездов на станциях.'
+  Введите 11, что бы просмотреть список станций и список поездов на станциях.
+  Введите 0, что бы выйти из программы.'
 
+  loop do
+    print 'Введите № команды:'
+    user_input = gets.chomp.to_i
 
+    case user_input
+    when 1 then create_station
+    when 2 then create_train
+    when 3 then create_route
+    when 4 then add_station_in_route
+    when 5 then remove_station_frome_route
+    when 6 then add_train_route
+    when 7 then add_wagons_to_train
+    when 8 then remoove_wagons_from_train
+    when 9 then train_go_forvard
+    when 10 then train_go_back
+    when 11 then show_list_stations_and_trains_at_station
+    when 0 then exit
+    else
+      puts 'Вы ввели неверный № команды! Пожалуйста, введите № команды из списка.'
+    end
+  end
 end
