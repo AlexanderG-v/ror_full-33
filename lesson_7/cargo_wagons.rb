9815a6ee88ab3c87d25d44bb4def59b5a0baa059
AlexanderG-v <gorelikov.alexander@gmail.com>
2021-10-05 16:55:02 +0300

    mod class PassengerWagons and CargoWagons

--- lesson_7/cargo_wagons.rb
+++ lesson_7/cargo_wagons.rb
@@ -4,9 +4,20 @@ require_relative 'modules/module_manufacturer'
 class CargoWagons
   include Manufacturer
 
-  attr_reader :type_wagon
+  attr_accessor :filled_volume
+  attr_reader :type_wagon, :volume
 
-  def initialize
+  def initialize(volume)
+    @volume = volume
+    @filled_volume = 0
     @type_wagon = 'cargo'
+  end
+
+  def load_volume(volume)
+    self.filled_volume += volume if volume + @filled_volume <= @volume
+  end
+
+  def free_volume
+    @volume - @filled_volume
   end
 end
--- lesson_7/passenger_wagons.rb
+++ lesson_7/passenger_wagons.rb
@@ -4,9 +4,22 @@ require_relative 'modules/module_manufacturer'
 class PassengerWagons
   include Manufacturer
 
-  attr_reader :type_wagon
+  attr_accessor :seats_taken
+  attr_reader :type_wagon, :count_seats
 
-  def initialize
+  def initialize(count_seats)
+    @count_seats = count_seats
+    @seats_taken = 0
     @type_wagon = 'passenger'
   end
+
+  def add_seat
+    self.seats_taken += 1 if @seats_taken < @count_seats
+  end
+
+  def free_seats
+    @count_seats - @seats_taken
+  end
+
+  
 end
