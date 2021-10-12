2b736b33d4feb10ed3209c6bc1184d5fb1442a28
AlexanderG-v <gorelikov.alexander@gmail.com>
2021-10-12 13:49:37 +0300

    corrected

--- lesson_9/modules/module_validation.rb
+++ lesson_9/modules/module_validation.rb
@@ -11,30 +11,36 @@ module Validation
 
     def validate(name, type, *options)
       self.check ||= []
-      self.check.push(name, type, options)
+      self.check.push({ name: name, type: type, options: options })
     end
   end
 
   module InstanceMethods
     def validate!
-      self.class.check.each do |name, type, options|
-        var = instance_variable_get("@#{name}".to_sym)
-        case type
-        when :presence
-          raise 'Не может быть пустым!' if var.nil? && var.empty?
-        when :format
-          raise 'Не правильный формат!' unless var =~ options.first
-        when :type
-          raise 'Не соответствует классу!' unless var.instance_of?(options.first)
-        end
+      self.class.check.each do |value|
+        argument_value = instance_variable_get("@#{value[:name]}")
+        method_name = "validate_#{value[:type]}"
+        send(method_name, argument_value, value[:options])
       end
     end
-  end
 
-  def valid?
-    validate!
-    true
-  rescue RuntimeError, ArgumentError
-    false
+    def validate_presence(name)
+      raise 'Не может быть пустым!' if name.nil? && name.empty?
+    end
+
+    def validate_format(name, _format)
+      raise 'Не правильный формат!' unless name =~ formate
+    end
+
+    def validate_type(name, type)
+      raise 'Не соответствует классу!' unless name.instance_of?(type)
+    end
+
+    def valid?
+      validate!
+      true
+    rescue RuntimeError, ArgumentError
+      false
+    end
   end
 end
